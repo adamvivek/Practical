ab545554311e6b2ea2133a5f7ac6962a703b8f73
Vivek Adam <70575628+adamvivek@users.noreply.github.com>
2023-03-16 11:56:13 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,21 @@
+class Unary{
+public static void main(String args[])
+{
+
+int a=10;
+int b=10;
+
+System.out.println(a++);
+System.out.println(++a);
+System.out.println(a--);
+System.out.println(--a);
+
+System.out.println(a++ + ++b);
+System.out.println(++a + ++b);
+System.out.println(b++ + a++);
+
+System.out.println(a-- - --b);
+System.out.println(--a - --b);
+System.out.println(b-- - a--);
+
+}}
