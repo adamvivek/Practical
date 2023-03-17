2aa8b6a5cc9f80493f4ac39bba3a541e6ccd8b61
Vivek Adam <70575628+adamvivek@users.noreply.github.com>
2023-03-17 12:07:22 +0530

    Add files via upload

---
+++ Simpleifelse.java
@@ -0,0 +1,15 @@
+class Simpleifelse {
+public static void main(String args[])
+{
+int x=5;
+if(x>0)
+{
+System.out.println("x is greater than 0");
+}
+
+else
+{
+System.out.println("x is less than 0");
+
+}
+}}
